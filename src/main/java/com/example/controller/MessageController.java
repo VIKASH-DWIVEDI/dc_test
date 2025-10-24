8a276b9cd8cfb914f6d23e8bf8352125e7fe2d18
vikash <dwivedivikas488@gmail.com>
2025-10-24 21:56:21 +0530

    first commit

---
+++ src/main/java/com/example/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+	@GetMapping("/message")
+	public String getMsg() {
+		return "Hello World";
+	}
+}
